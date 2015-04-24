f036ea421ad091edfbe44f6ad251dba2c159dba1
cding <cding@nationalfibre.net>
2015-04-24 10:41:16 -0400

    close popup

--- clearHistory/clear.js
+++ clearHistory/clear.js
@@ -1,9 +1,18 @@
+
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('bbb');
 
     chrome.tabs.query({'active': true}, function(tabs) {
         chrome.tabs.update(tabs[0].id, {url: 'chrome://settings/clearBrowserData'});
     });
-
+    window.close();
     return;
+
+    // Add "history" in manifest.json->permissions to enable history API
     // chrome.history.deleteAll(function (){});
 }, false);
+
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     console.log('aaa');
+//     console.log(tab.url);
+// });
